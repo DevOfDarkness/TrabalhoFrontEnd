9d3626af535110377e38f30ccfbd5661b0963627
Wesley Lima <wesleyjoga500@gmail.com>
2019-02-03 14:42:11 -0200

    Trabalho de Front End

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { DestinosComponent } from './pages/destinos/destinos.component';
+import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'contato', component: ContatoComponent},
+  {path: 'sobre-nos', component: SobreNosComponent},
+  {path: 'destinos', component: DestinosComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContatoComponent } from './pages/contato/contato.component';
+import { DestinosComponent } from './pages/destinos/destinos.component';
+import { SobreNosComponent } from './pages/sobre-nos/sobre-nos.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContatoComponent,
+    DestinosComponent,
+    SobreNosComponent,
+    FooterComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterializeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
